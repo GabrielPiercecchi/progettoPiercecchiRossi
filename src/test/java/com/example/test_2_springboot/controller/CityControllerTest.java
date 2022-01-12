aba8352ee15ab1a8afc613dde1465ca4b0931799
GabrielP <s1097612@studenti.univpm.it>
2022-01-12 16:40:20 +0100

    Creazione di un ulteriore Test

---
+++ src/test/java/com/example/test_2_springboot/controller/CityControllerTest.java
@@ -0,0 +1,32 @@
+package com.example.test_2_springboot.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CityControllerTest {
+
+    @Test
+    void getCity() {
+    }
+
+    @Test
+    void updateCity() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void createCity() {
+    }
+
+    @Test
+    void citiesName() {
+    }
+
+    @Test
+    void getCityData() {
+    }
+}
