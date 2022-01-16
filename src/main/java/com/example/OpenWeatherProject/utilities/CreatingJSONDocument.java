c34f54da427bfd5cd0a6ff3099a6d9ce4ad43da3
GabrielP <s1097612@studenti.univpm.it>
2022-01-16 18:15:00 +0100

    Miglioramento indentazione

--- src/main/java/com/example/OpenWeatherProject/utilities/CreatingJSONDocument.java
+++ src/main/java/com/example/OpenWeatherProject/utilities/CreatingJSONDocument.java
@@ -1,9 +1,6 @@
 package com.example.OpenWeatherProject.utilities;
 
 
-import com.example.OpenWeatherProject.filters.CityFilters;
-import com.example.OpenWeatherProject.filters.ControlFiltersImpl;
-import com.example.OpenWeatherProject.stats.CityStats;
 import org.json.simple.JSONObject;
 
 import java.io.BufferedWriter;
@@ -15,8 +12,6 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
 public class CreatingJSONDocument {
 
@@ -24,8 +19,6 @@ public class CreatingJSONDocument {
     // private final AtomicLong counter = new AtomicLong();
 
     public void fileWriter(String name, String formattedDate, double temp, double feels_like, double temp_min, double temp_max) throws IOException {
-
-        CityFilters cityFilters = new CityFilters(null, null, 0, 0, 0, 0);
 
         JSONObject citiesObj = new JSONObject();
         citiesObj.put("Call N°", ++cityCounter);
@@ -44,11 +37,13 @@ public class CreatingJSONDocument {
             File file = new File("FileCities.json");
             BufferedWriter bufferedWriter;
 
+            //--> Crea il file se non esiste
             if (!file.exists()) {
                 file.createNewFile();
                 System.out.println("JSON file created");
                 System.out.println();
             }
+
             // Saving previous data to a String variable
             Path path = Paths.get("FileCities.json");
             Charset charset = StandardCharsets.UTF_8;
@@ -63,7 +58,6 @@ public class CreatingJSONDocument {
 
             bufferedWriter.close();
             //System.out.println("--> File updated successfully.");
-
         } catch (IOException e) {
             System.out.println("ERROR");
             System.out.println("--> File not updated");
@@ -71,96 +65,3 @@ public class CreatingJSONDocument {
         }
     }
 }
-
-//--> Vecchia classe
-/*
- public class CreatingJSONDocument {
-
-     public static int cityCounter = 0;
-     private final AtomicLong counter = new AtomicLong();
-
-     public void fileWriter(String name, double temp, double feels_like, double temp_min, double temp_max) throws IOException {
-
-         //--> STATS/FILTRI su base oraria
-         //--> metodo da fare con attenzione (WORK IN PROGRES)
-
-         //--> Incremento cityCounter ad ogni chiamata del metodo
-         ++cityCounter;
-
-         LocalDateTime myDateObj = LocalDateTime.now();
-         DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
-         String formattedDate = myDateObj.format(myFormatObj);
-
-         JSONObject citiesObj = new JSONObject();
-         citiesObj.put("Name_" + cityCounter, name);
-         citiesObj.put("Time_" + cityCounter, formattedDate);
-
-         JSONObject listOfTemps = new JSONObject();
-         listOfTemps.put("temp_" + cityCounter, temp);
-         listOfTemps.put("feels_like_" + cityCounter, feels_like);
-         listOfTemps.put("temp_min_" + cityCounter, temp_min);
-         listOfTemps.put("temp_max_" + cityCounter, temp_max);
-
-         citiesObj.put("Main_" + cityCounter, listOfTemps);
-
-         //ObjectMapper mapper = new ObjectMapper();//--> legato al metodo secondario sottostante
-
-         try {
-             // Writing a file
-             File file = new File("FileCities.json");
-             BufferedWriter bufferedWriter = null;
-             if (!file.exists()) {
-                 file.createNewFile();
-                 System.out.println("JSON file created:");
-                 System.out.println("");
-             }
-
-             /*
-              //--> Metodo alternativo per la scrittura dei dati nel file JSON
-             // Create JSON
-             final String json = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(citiesObj.toJSONString());
-             // Content is appended (due to StandardOpenOption.APPEND)
-             Files.write(new File("FileCities.json").toPath(), Arrays.asList(json), StandardOpenOption.APPEND);
-             System.out.print(citiesObj);
- /*
-             bufferedWriter = new BufferedWriter(new FileWriter(new File("FileCities.json"), true));
-             bufferedWriter.write(MessageFormat.format("{0}\n", citiesObj.toJSONString()));
-             bufferedWriter.close();
-             //System.out.println();
-             //System.out.println("--> File updated successfully.");
-
-         } catch (IOException e) {
-             System.out.println("ERROR");
-             System.out.println("--> File not updated");
-             e.printStackTrace();
-         }
-     }
- }
-*/
-
-//--> Classe originaria
-/*
-public class CreatingJSONDocument {
-    public static void main(String args[]) {
-
-        //Creating a JSONObject object
-        JSONObject jsonObject = new JSONObject();
-
-        //Inserting key-value pairs into the json object
-        jsonObject.put("ID", "1");
-        jsonObject.put("First_Name", "Shikhar");
-        jsonObject.put("Last_Name", "Dhawan");
-        jsonObject.put("Date_Of_Birth", "1981-12-05");
-        jsonObject.put("Place_Of_Birth", "Delhi");
-        jsonObject.put("Country", "India");
-        try {
-            FileWriter file = new FileWriter(".\\JsonFile\\Cities.json");
-            file.write(jsonObject.toJSONString());
-            file.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        System.out.println("JSON file created: "+jsonObject);
-    }
-}
-*/
